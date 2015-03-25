5eae578fcdf6213cff351be5d971e7df5ff5dc63
Lexan <tolexan@gmail.com>
2015-03-25 18:20:11 +0400

    Ядро (бизнес-логика)
    + добавлен пакет Core

---
+++ RoadRingSim/RoadRingSim.Core/Domains/DomainObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadRingSim.Core.Domains
+{
+    abstract class DomainObject
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ RoadRingSim/RoadRingSim.Core/Domains/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadRingSim.Core.Domains
+{
+    class Role
+    {
+        public string rolename;
+        public List<User> Users {get; set; };
+    }
+}
